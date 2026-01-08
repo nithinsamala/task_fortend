d4d9ad0c29165a02c79a693e5f2c2433532b5992
nithinsamala <blkg896@gmail.com>
2026-01-08 20:42:44 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 
-const Home = () => {
+function Home() {
   return (
     <div className="home-container">
       <h1 className="welcome-title">Welcome to TaskTracker</h1>
+
       <p className="welcome-text">
-        Manage your daily tasks, keep track of progress and stay organized.
+        A simple app to manage your daily tasks and stay organized.
       </p>
-      <div className="cta-section">
-        <Link to="/tasks" className="cta-button">
-          Get Started
-        </Link>
-      </div>
+
+      <Link to="/tasks" className="cta-button">
+        Get Started
+      </Link>
     </div>
   );
-};
+}
 
 export default Home;
