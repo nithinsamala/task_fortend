a2ee1676b02543dcf26c1a333692ec0caaf8cdc9
nithinsamala <blkg896@gmail.com>
2026-01-08 20:20:49 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import "./navbar.css";
+import "./Navbar.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
